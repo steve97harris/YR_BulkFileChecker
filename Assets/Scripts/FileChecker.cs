ff4ac0ec379deb91be6a28ea0ee9413ad2507d8b
steve97harris <steve@thisisyr.com>
2020-10-15 17:23:37 +0100

    Buildable

--- Assets/Scripts/FileChecker.cs
+++ Assets/Scripts/FileChecker.cs
@@ -14,6 +14,17 @@ namespace DefaultNamespace
     {
         private string _repoPath = "";
         private string _csvFilePath = "";
+        private string _columnHeader = "";
+        private string _fileType = "";
+
+        private static GameObject _mainCanvas = null;
+
+        private void Start()
+        {
+            _mainCanvas = Resources.Load<GameObject>(@"Prefabs\CanvasA");
+            _mainCanvas = Instantiate(_mainCanvas);
+            GetPlayerPrefs();
+        }
 
         public void InputValueRepoPath(string path)
         {
@@ -24,80 +35,131 @@ namespace DefaultNamespace
         {
             _csvFilePath = path;
         }
+        
+        public void InputValueColumnHeader(string columnHeader)
+        {
+            _columnHeader = columnHeader;
+        }
+
+        public void InputValueFileType(string fileType)
+        {
+            _fileType = fileType;
+        }
+
+        private void DisplayError(string error)
+        {
+            var errorObj = FindSingleObjectByName("error101");
+            if (errorObj == null)
+            {
+                errorObj = Resources.Load<GameObject>(@"Prefabs\error101");
+                errorObj = Instantiate(errorObj, _mainCanvas.transform);
+            }
+
+            if (errorObj.GetComponent<TMP_Text>().text == "_error_")
+                errorObj.GetComponent<TMP_Text>().text = error;
+            else
+                errorObj.GetComponent<TMP_Text>().text += Environment.NewLine + error;
+
+            errorObj.SetActive(true);
+        }
+
+        public void ExitErrorMessage()
+        {
+            var errorObj = FindSingleObjectByName("error101");
+            DestroyImmediate(errorObj, true);
+        }
 
         public void CheckFiles()
         {
+            SetPlayerPrefs();
+            
             var csvFileList = LoadCsvFileViaPath(_csvFilePath);
 
             if (csvFileList.Count == 0)
             {
                 Debug.LogError("CsvFile returned empty");
+                DisplayError("CsvFile returned empty, try another path");
                 return;
             }
 
-            var artworkIndex = GetArtworkIndex(csvFileList);
+            var artworkIndex = GetColumnIndex(csvFileList, _columnHeader);
             if (artworkIndex == -1)
             {
-                Debug.LogError("No column found with header: ARTWORK_NAME");
+                Debug.LogError("No column found with header: " + _columnHeader);
+                DisplayError("No column found with header: " + _columnHeader);
                 return;
             }
+
+            var subCategoryIndex = GetColumnIndex(csvFileList, "SUB_CATERGORY");
             
-            var artworkNameList = GetArtworkNameList(csvFileList, artworkIndex);
-            var files = GetRepositoryFileNames(_repoPath, "*.png");
+            var artworkNameList = GetArtworkNameList(csvFileList, artworkIndex, subCategoryIndex);
+
+            var fileType = "*." + _fileType;
+            var files = GetRepositoryFileNames(_repoPath, fileType);
 
             for (int i = 0; i < files.Length; i++)
             {
                 Debug.Log(files[i]);
             }
             
-            var content = FindSingleObjectByName("Content");
-            foreach (Transform child in content.transform)
-            {
-                Destroy(child.gameObject);
-            }
+            var content = FindSingleObjectByName("ContentArtwork");
+            DestroyImmediate(content.transform.GetChild(0).gameObject, true);
 
             var textGameObj = Resources.Load<GameObject>("Prefabs/ArtworkCheckerTextTemplate");
             for (int i = 0; i < artworkNameList.Count; i++)
             {
-                textGameObj.GetComponent<TMP_Text>().text = artworkNameList[i] + ": ";
-                
+                var artworkName = artworkNameList[i].Split(',')[0];
+                textGameObj.GetComponent<TMP_Text>().text = artworkName + ": ";
+
                 CheckArtwork(files, artworkNameList, i, textGameObj, "_MSK");
                 CheckArtwork(files, artworkNameList, i, textGameObj, "_PRN");
                 CheckArtwork(files, artworkNameList, i, textGameObj, "_SCR");
                 CheckArtwork(files, artworkNameList, i, textGameObj, "_THM");
 
+                var subCategory = artworkNameList[i].Split(',')[1];
+                textGameObj.GetComponent<TMP_Text>().text += " " + "(" + subCategory + ")";
+
                 Instantiate(textGameObj, content.transform);
             }
         }
 
+        
+
         private string[] GetRepositoryFileNames(string repoPath, string fileType)
         {
             var repoFiles = Directory.GetFiles(repoPath, fileType, SearchOption.AllDirectories);
+            
+            fileType = fileType.Replace("*", "");
+            
             var fileNames = new List<string>();
             for (int i = 0; i < repoFiles.Length; i++)
             {
                 var fileName = Path.GetFileName(repoFiles[i]);
-                if (fileName.EndsWith(".png"))
-                    fileName = fileName.Replace(".png", "");
+                
+                if (fileName.EndsWith(fileType))
+                    fileName = fileName.Replace(fileType, "");
                 
                 fileNames.Add(fileName);
             }
+            
             return fileNames.ToArray();
         }
 
-        private List<string> GetArtworkNameList(List<string> csvFileList, int artworkIndex)
+        private List<string> GetArtworkNameList(List<string> csvFileList, int artworkIndex, int subCategoryIndex)
         {
             var artworkNameList = new List<string>();
             for (int i = 0; i < csvFileList.Count; i++)
             {
                 var split = csvFileList[i].Split(',');
                 var artworkName = split[artworkIndex];
-                if (artworkName == "ARTWORK_NAME")
+                var subCategory = split[subCategoryIndex];
+                if (artworkName == _columnHeader)
                     continue;
 
                 if (!artworkNameList.Contains(artworkName))
                 {
-                    artworkNameList.Add(artworkName);
+                    var str = artworkName + "," + subCategory;
+                    artworkNameList.Add(str);
                 }
                 else
                 {
@@ -108,25 +170,31 @@ namespace DefaultNamespace
             return artworkNameList;
         }
 
-        private int GetArtworkIndex(List<string> csvFileList)
+        private int GetColumnIndex(List<string> csvFileList, string columnHeader)
         {
             var headers = csvFileList[0];
             var headerSplit = headers.Split(',');
-            var artworkIndex = -1;
+            var columnIndex = -1;
 
             for (int i = 0; i < headerSplit.Length; i++)
             {
-                if (headerSplit[i] == "ARTWORK_NAME")
+                if (headerSplit[i] == columnHeader)
                 {
-                    artworkIndex = i;
+                    columnIndex = i;
                 }
             }
 
-            return artworkIndex;
+            return columnIndex;
         }
-        
+
         private List<string> LoadCsvFileViaPath(string filePath)
         {
+            if (!File.Exists(filePath))
+            {
+                DisplayError("CSV File Not Found.");
+                return null;
+            }
+            
             var reader = new StreamReader(File.OpenRead(filePath));
             List<string> searchList = new List<string>();
             while (!reader.EndOfStream)
@@ -146,27 +214,78 @@ namespace DefaultNamespace
 
         private void CheckArtwork(string[] files, List<string> artworkNameList, int i, GameObject textGameObj, string imageType)
         {
-            var filesWithCurrentArtworkName = files.Select(x => x).Where(x => x == artworkNameList[i] + imageType).ToArray();
+            var artworkNameSplit = artworkNameList[i].Split(',');
+            var artworkName = artworkNameSplit[0];
+            var filesWithCurrentArtworkName = files.Select(x => x).Where(x => x == artworkName + imageType).ToArray();
 
             switch (filesWithCurrentArtworkName.Length)
             {
                 case 0:
-                    Debug.LogError("File could not be found with following artwork name: " + artworkNameList[i] + imageType);
+                    // Debug.LogError("File could not be found with following artwork name: " + artworkName + imageType);
                     textGameObj.GetComponent<TMP_Text>().text +=
                         "<color=red>" + imageType + "</color>, ";
                     break;
                 case 1:
-                    Debug.Log("Success, file found!" + Environment.NewLine + " ARTWORK_NAME: " + artworkNameList[i] + imageType + Environment.NewLine + " FileName: " + filesWithCurrentArtworkName[0]);
+                    // Debug.Log("Success, file found!" + Environment.NewLine + " " + _columnHeader + ": " + artworkName + imageType + Environment.NewLine + " FileName: " + filesWithCurrentArtworkName[0]);
                     textGameObj.GetComponent<TMP_Text>().text += "<color=green>"+ imageType +"</color>, ";
                     break;
             }
             if (filesWithCurrentArtworkName.Length > 1)
             {
-                Debug.LogError("Multiple files found with the following name: " + artworkNameList[i] + imageType);
+                // Debug.LogError("Multiple files found with the following name: " + artworkName + imageType);
                 textGameObj.GetComponent<TMP_Text>().text +=
-                    "<color=orange>" + imageType + "</color>, ";
+                    "<color=yellow>" + imageType + "</color>, ";
             }
         }
+
+        #region InputField
+
+        private void SetPlayerPrefs()
+        {
+            PlayerPrefs.SetString("COLUMN_HEADER", _columnHeader);
+            PlayerPrefs.SetString("FILE_TYPE", _fileType);
+            PlayerPrefs.SetString("REPO_PATH", _repoPath);
+            PlayerPrefs.SetString("CSV_PATH", _csvFilePath);
+        }
+
+        private void GetPlayerPrefs()
+        {
+            if (PlayerPrefs.GetString("COLUMN_HEADER") != null)
+            {
+                var columnHeader = PlayerPrefs.GetString("COLUMN_HEADER");
+                Debug.LogError(columnHeader);
+                RestorePreviousInputFieldValue("InputFieldC", columnHeader);
+            }
+            if (PlayerPrefs.GetString("FILE_TYPE") != null)
+            {
+                var fileType = PlayerPrefs.GetString("FILE_TYPE");
+                RestorePreviousInputFieldValue("InputFieldD", fileType);
+            }
+            if (PlayerPrefs.GetString("REPO_PATH") != null)
+            {
+                var repoPath = PlayerPrefs.GetString("REPO_PATH");
+                RestorePreviousInputFieldValue("InputFieldA", repoPath);
+            }
+            if (PlayerPrefs.GetString("CSV_PATH") != null)
+            {
+                var csvPath = PlayerPrefs.GetString("CSV_PATH");
+                RestorePreviousInputFieldValue("InputFieldB", csvPath);
+            }
+        }
+        
+
+        private void RestorePreviousInputFieldValue(string inputFieldObjName, string storedValue)
+        {
+            var field = FindSingleObjectByName(inputFieldObjName);
+            var textObj = field.transform.GetChild(0).gameObject;
+
+            Debug.LogError(field.name);
+            Debug.LogError(textObj.name);
+            
+            textObj.GetComponent<TMP_InputField>().text = storedValue;
+        }
+
+        #endregion
     }
 }
 
